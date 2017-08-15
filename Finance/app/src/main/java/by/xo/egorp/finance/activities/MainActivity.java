fd9883ab8957e309a983c18420fd550537c162b5
Egor Pavlenko <egorpavlenko18@gmail.com>
2017-08-15 22:02:13 +0300

    Spash screen

--- Finance/app/src/main/java/by/xo/egorp/finance/activities/MainActivity.java
+++ Finance/app/src/main/java/by/xo/egorp/finance/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package by.xo.egorp.finance;
+package by.xo.egorp.finance.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import by.xo.egorp.finance.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
